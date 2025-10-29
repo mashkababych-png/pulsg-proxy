6c4248f239ca3dd630e00e31306b86836fc44a04
mashkababych-png <mashka.babych@gmail.com>
2025-10-29 03:19:06 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 console.log('PULS G Proxy active ⚡️')
+// app.js
+const express = require('express');
+const app = express();
+
+// головний маршрут
+app.get('/', (req, res) => {
+  res.send('PULS G Proxy is active ⚡️');
+});
+
+// Render надає порт через змінну середовища PORT
+const PORT = process.env.PORT || 10000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
